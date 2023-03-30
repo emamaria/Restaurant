f35a40b6376b054716615a24cd862d678ede5dd5
emamaria <emascript@gmail.com>
2023-03-30 12:40:31 +0200

    home basic style

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section class="par1">
+      <div class="par_text">
+        <h1 class="title">Delizioso</h1>
+        <h2 class="sub_title">Restaurante Italiano</h2>
+      </div>
+    </section>
+    <section class="par2">
+      <div class="par_text">
+        <p class="menu">Nuestra carta</p>
+        <button>Carta</button>
+      </div>
+    </section>
+    <section class="par3">
+      <div class="par_text">
+        <p class="reservation">Haz tu reseva</p>
+        <button>Reserva</button>
+      </div>
+    </section>
+    </>
   )
 }
 
