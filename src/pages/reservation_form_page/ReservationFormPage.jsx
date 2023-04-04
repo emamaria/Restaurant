0abeb490b0af6930edbd528978b7e5a37ede5690
emamaria <emascript@gmail.com>
2023-04-04 19:15:41 +0200

    changed form

--- src/pages/reservation_form_page/ReservationFormPage.jsx
+++ src/pages/reservation_form_page/ReservationFormPage.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import {useForm}  from '../../customHook/form'
+
+
 import { useParams } from 'react-router-dom'
 import {adress} from '../../data/adress/adress.js'
+import { useState } from 'react';
 
 
 export const ReservationFormPage = () => {
@@ -13,13 +14,20 @@ export const ReservationFormPage = () => {
 
     console.log(currentAdress)
 
-    const {form, inputOnChange} = useForm({
-        date: "",
-        time: "",
-        personsNum: "",
-        email: "",
-    })
+ 
 
+     const [form, setForm] = useState({
+      date: "",
+      time: "",
+      personsNum: "",
+      email: "",
+     })
+
+     const inputOnChange = (e) =>{
+         const {name, value} = e.target
+
+         setForm({...form, [name]:value})
+     }
    
 
     const {date, time, personsNum, email} = form;
@@ -39,13 +47,16 @@ export const ReservationFormPage = () => {
         </fieldset>
         <fieldset  className='second_fieldset section_space'>
     <label htmlFor="email">Your email:</label> 
-    <input type="email" id="email"className="form_control" placeholder="email" name="email"value={email} onChange={inputOnChange} />
+    <input type="email" id="email" className="form_control" name="email" value={email} onChange={inputOnChange} />
      <label htmlFor="date">Select a date:</label>
-     <input type="date" id="date"className="form_control" min={`${new Date().getDate()}`} name="date" value={date} onChange={inputOnChange}/>
+     <input type="date" id="date" className="form_control"  min={`${new Date().getDate()}`} max="2023-12-31" name="date" value={date} onChange={inputOnChange}/>
       <label htmlFor="quantity">Persons Number:</label>
-      <input type="number" className="form_control" placeholder="persons number"min="1" max="10"id="quantity"name="personsNum"value={personsNum}onChange={inputOnChange} />
+      <input type="number" className="form_control"  placeholder="persons number" min="1" max="10" id="quantity" name="personsNum" value={personsNum} onChange={inputOnChange} />
       <label htmlFor="time">Select a time:</label>
-       <input type="time" className="form_control" placeholder="what time" id="time" name="time" value={time} onChange={inputOnChange}/>
+       <select className="form_control" placeholder="what time"   id="time" name="time" value={time} onChange={inputOnChange}>
+       <option value="value1" >Value 1</option>
+       <option value="value2">Value 2</option>
+       </select>
       </fieldset>
 
     <button className="reservation_button">Reserva</button>
