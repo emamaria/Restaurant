9a8583ad94786c704da18852829e4002fa9cf39d
emamaria <emascript@gmail.com>
2023-04-04 17:28:42 +0200

    reservation form

--- src/pages/reservation_form_page/ReservationFormPage.jsx
+++ src/pages/reservation_form_page/ReservationFormPage.jsx
@@ -29,61 +29,29 @@ export const ReservationFormPage = () => {
     console.log(date, time, personsNum, email)
   return (
     <main className="reservation_form_container">
-    <h2 className="reservation_form__title">Reservar Mesa</h2>
-    <form>
-        <h3>{sitio}</h3>
-        <fieldset>
+    <h2 className="reservation_form__title ">Reserva Mesa</h2>
+    <form className="reservation_form">
+        <h3 className="form_title section_space">{sitio[0].toUpperCase() + sitio.substring(1)}</h3>
+        <fieldset className='first_fieldset section_space'>
             <p>{currentAdress.adress}</p>
             <p>{currentAdress.openTime}</p>
             <p>{currentAdress.tel}</p>
         </fieldset>
-        <fieldset>
-   <label for="email">Your email:</label> 
-    <input
-     type="email" 
-     id="email"
-     className="form_control" 
-     placeholder="email"
-     name="email"
-     value={email}
-     onChange={inputOnChange}
-      />
-     <label for="date">Select a date:</label>
-    <input
-     type="date" 
-     id="date"
-     className="form_control" 
-     min={`${new Date().getDate()}`}
-     name="date"
-     value={date}
-     onChange={inputOnChange}
-      />
-       <label for="quantity">Persons Number:</label>
-      <input
-     type="number" 
-     className="form_control" 
-     placeholder="persons number"
-     min="1" max="10"
-     id="quantity"
-     name="personsNum"
-     value={personsNum}
-     onChange={inputOnChange}
-      />
-      <label for="time">Select a time:</label>
-       <input
-     type="time" 
-     className="form_control" 
-     placeholder="what time"
-     id="time"
-     name="time"
-     value={time}
-     onChange={inputOnChange}
-      />
-    </fieldset>
+        <fieldset  className='second_fieldset section_space'>
+    <label htmlFor="email">Your email:</label> 
+    <input type="email" id="email"className="form_control" placeholder="email" name="email"value={email} onChange={inputOnChange} />
+     <label htmlFor="date">Select a date:</label>
+     <input type="date" id="date"className="form_control" min={`${new Date().getDate()}`} name="date" value={date} onChange={inputOnChange}/>
+      <label htmlFor="quantity">Persons Number:</label>
+      <input type="number" className="form_control" placeholder="persons number"min="1" max="10"id="quantity"name="personsNum"value={personsNum}onChange={inputOnChange} />
+      <label htmlFor="time">Select a time:</label>
+       <input type="time" className="form_control" placeholder="what time" id="time" name="time" value={time} onChange={inputOnChange}/>
+      </fieldset>
 
-    </form>
+    <button className="reservation_button">Reserva</button>
+ </form>
 
-
+ 
 
   </main>
   )
