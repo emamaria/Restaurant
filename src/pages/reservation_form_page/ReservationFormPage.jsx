fac1e3b3d742648c36aa53992d6cb5f9ce48606e
emamaria <emascript@gmail.com>
2023-04-05 14:17:10 +0200

    ok form select

--- src/pages/reservation_form_page/ReservationFormPage.jsx
+++ src/pages/reservation_form_page/ReservationFormPage.jsx
@@ -6,7 +6,13 @@ import { useState } from 'react';
 
 
 export const ReservationFormPage = () => {
+
   
+   let reservationTime = ["13:15","13:30","13:45","13:50","14:05","14:20","14:35","14:50","19:15",
+    "19:30","19:45","20:00","20:15","20:30","20:45","21:00","21:15","21:30","21:45","22:00",
+    "22:15","22:30","22:45"]
+
+
     let {sitio} = useParams()
 
     let currentAdress;
@@ -20,7 +26,7 @@ export const ReservationFormPage = () => {
       date: "",
       time: "",
       personsNum: "",
-      email: "",
+      email: ""
      })
 
      const inputOnChange = (e) =>{
@@ -47,19 +53,19 @@ export const ReservationFormPage = () => {
         </fieldset>
         <fieldset  className='second_fieldset section_space'>
     <label htmlFor="email">Your email:</label> 
-    <input type="email" id="email" className="form_control" name="email" value={email} onChange={inputOnChange} />
+    <input required type="email" id="email" className="form_control" placeholder="tu email"  name="email" value={email} onChange={inputOnChange} />
      <label htmlFor="date">Select a date:</label>
-     <input type="date" id="date" className="form_control"  min={`${new Date().getDate()}`} max="2023-12-31" name="date" value={date} onChange={inputOnChange}/>
-      <label htmlFor="quantity">Persons Number:</label>
-      <input type="number" className="form_control"  placeholder="persons number" min="1" max="10" id="quantity" name="personsNum" value={personsNum} onChange={inputOnChange} />
+     <input  required type="date" id="date" className="form_control"  min={`${new Date().getDate()}`} max="2023-12-31" name="date" value={date} onChange={inputOnChange}/>
+      <label htmlFor="quantity">Persons:</label>
+      <select required className="form_control" id="quantity" name="personsNum" value={personsNum} onChange={inputOnChange} >
+     { [...Array(10).keys()].map((index)=> <option value={index + 1} key={index} > {index + 1} Pax</option>)}
+      </select>
       <label htmlFor="time">Select a time:</label>
-       <select className="form_control" placeholder="what time"   id="time" name="time" value={time} onChange={inputOnChange}>
-       <option value="value1" >Value 1</option>
-       <option value="value2">Value 2</option>
+       <select required className="form_control" placeholder="what time"   id="time" name="time" value={time} onChange={inputOnChange}>
+        {reservationTime.map((time, index) =>  <option key={index} value={time} >{time}</option>)}
        </select>
       </fieldset>
-
-    <button className="reservation_button">Reserva</button>
+ <button type="submit" className="reservation_button">Reserva</button>
  </form>
 
  
